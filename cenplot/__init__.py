7e4e83e857cc498abe7d36866ea5e100cc19fdf6
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2025-01-26 05:21:12 -0500

    Add MIT license. Fix imports.

--- cenplot/__init__.py
+++ cenplot/__init__.py
@@ -1,9 +1,24 @@
-import draw
-import io
-import track
+import logging
 
+from .draw import draw_hor_w_ort, draw_label, draw_self_ident, draw_values, plot_one_cen
+from .io import read_bed9, read_bed_hor, read_bed_identity, read_bed_label
+from .track import Track, TrackOption, TrackPosition
+
+__author__ = "Keith Oshima (oshimak@pennmedicine.upenn.edu)"
+__license__ = "MIT"
 __all__ = [
-    "draw",
-    "io",
-    "track",
+    "plot_one_cen",
+    "draw_hor_w_ort",
+    "draw_label",
+    "draw_self_ident",
+    "draw_values",
+    "read_bed9",
+    "read_bed_hor",
+    "read_bed_identity",
+    "read_bed_label",
+    "Track",
+    "TrackOption",
+    "TrackPosition",
 ]
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
