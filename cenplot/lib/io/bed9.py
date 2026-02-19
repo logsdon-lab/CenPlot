35c08d7fb759f269db616988cbe41962b4fe7080
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2026-02-19 12:06:25 -0500

    Fix filter not working for all conditions.

--- cenplot/lib/io/bed9.py
+++ cenplot/lib/io/bed9.py
@@ -36,24 +36,59 @@ def read_bed9(infile: str | TextIO, *, chrom: str | None = None) -> pl.DataFrame
             chrom_no_coords = None
             chrom_st, chrom_end = None, None
 
+        def expr_chrom_coords(
+            expr_no_coords: pl.Expr, expr_coords: pl.Expr, expr_otherwise: pl.Expr
+        ) -> pl.Expr:
+            return (
+                pl.when(pl.col("chrom").eq(chrom_no_coords))
+                .then(expr_no_coords)
+                .when(pl.col("chrom").eq(chrom))
+                .then(expr_coords)
+                .otherwise(expr_otherwise)
+            )
+
+        # Chrom coordinates can be one of three states:
+        # 1. chr1:0-10:0-5
+        # 2. chr1:0-10
+        # 3. chr1
+        # We assume if an exact match for chrom_no_coords is found (1), the user wants to trim to some coordinates.
+        # Coordinate are right split once.
         if chrom_no_coords and chrom_st and chrom_end:
             df_filtered = (
-                df.filter(pl.col("chrom") == chrom_no_coords)
+                df.filter(
+                    expr_chrom_coords(
+                        pl.col("chrom") == chrom_no_coords,
+                        pl.col("chrom") == chrom,
+                        True,
+                    )
+                )
                 .with_columns(
-                    pl.col("chrom_st").clip(chrom_st, chrom_end),
-                    pl.col("chrom_end").clip(chrom_st, chrom_end),
+                    chrom_st=expr_chrom_coords(
+                        pl.col("chrom_st").clip(chrom_st, chrom_end),
+                        pl.col("chrom_st"),
+                        pl.col("chrom_st"),
+                    ),
+                    chrom_end=expr_chrom_coords(
+                        pl.col("chrom_end").clip(chrom_st, chrom_end),
+                        pl.col("chrom_end"),
+                        pl.col("chrom_end"),
+                    ),
                 )
                 # Remove null intervals created by clipping to boundaries
                 .filter(
-                    ~(
-                        (
-                            pl.col("chrom_st").eq(chrom_st)
-                            & pl.col("chrom_st").eq(chrom_end)
-                        )
-                        | (
-                            pl.col("chrom_end").eq(chrom_st)
-                            & pl.col("chrom_end").eq(chrom_end)
-                        )
+                    expr_chrom_coords(
+                        ~(
+                            (
+                                pl.col("chrom_st").eq(chrom_st)
+                                & pl.col("chrom_st").eq(chrom_end)
+                            )
+                            | (
+                                pl.col("chrom_end").eq(chrom_st)
+                                & pl.col("chrom_end").eq(chrom_end)
+                            )
+                        ),
+                        True,
+                        True,
                     )
                 )
                 .collect()
